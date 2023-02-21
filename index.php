22687766c23fc67a00ef2cbc752c989ad6713bd4
Spectrum5 <toskusergio@gmail.com>
2023-02-21 23:00:45 +0100

    Aggiunto bonus del filtro parcheggio

--- index.php
+++ index.php
@@ -1,44 +1,70 @@
 <?php
 
-$hotels = [
+    $hotels = [
 
-    [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
-    ],
-    [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
-    ],
-    [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
-    ],
-    [
-        'name' => 'Hotel Bellavista',
-        'description' => 'Hotel Bellavista Descrizione',
-        'parking' => false,
-        'vote' => 5,
-        'distance_to_center' => 5.5
-    ],
-    [
-        'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 50
-    ],
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
 
-];
+    ];
+
+    $parkingFilter = null;
+    if (isset($_GET['parking'])) {
+        $parkingFilter = $_GET['parking'];
+    }
+
+    $filteredHotels = [];
+    foreach ($hotels as $hotel) {
+        if (
+            $parkingFilter == ''
+            ||
+            (
+                $parkingFilter == '1'
+                &&
+                $hotel['parking'] == true
+            )
+            ||
+            (
+                $parkingFilter == '0'
+                &&
+                $hotel['parking'] == false
+            )
+        ) {
+            $filteredHotels[] = $hotel;
+        }
+    }
 
 ?>
 
@@ -58,56 +84,69 @@ $hotels = [
 </head>
 
 <body>
-    <form action="" method="get">
-        <label for="parking">Parcheggio</label>
-            <select name="parking" id="parking">
-                <option value="">Tutti</option>
-                <option value="si">Si</option>
-                <option value="no">No</option>
-            </select>
-        <label for="vote">Voto</label>
-        
-        <input type="number" name="vote" id="vote" min="1" max="5">
 
-        <button type="submit">Filtra</button>
-    </form>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <form action="" method="GET">
+                        <label for="parking">Parcheggio</label>
+                        <select name="parking" id="parking" class="form-select">
+                            <option value="">Tutti</option>
+                            <option value="1">Si</option>
+                            <option value="0">No</option>
+                        </select>
+                        <label for="vote">Voto</label>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#N</th>
-                <th scope="col">Name</th>
-                <th scope="col">Description</th>
-                <th scope="col">Parking</th>
-                <th scope="col">Vote</th>
-                <th scope="col">Distance to center</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($hotels as $key => $hotel) { ?>
-                <tr>
-                    <th scope="row">
-                        <?php echo $key ?>
-                    </th>
-                    <td>
-                        <?php echo $hotel['name'] ?>
-                    </td>
-                    <td>
-                        <?php echo $hotel['description'] ?>
-                    </td>
-                    <td>
-                        <?php echo $hotel['parking'] ?>
-                    </td>
-                    <td>
-                        <?php echo $hotel['vote'] ?>
-                    </td>
-                    <td>
-                        <?php echo $hotel['distance_to_center'] ?>
-                    </td>
-                </tr>
-            <?php  } ?>
-        </tbody>
-    </table>
+                        <input type="number" name="vote" id="vote" min="1" max="5">
+
+                        <button type="submit">
+                            Filtra
+                        </button>
+                    </form>
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#N</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Parking</th>
+                                <th scope="col">Vote</th>
+                                <th scope="col">Distance to center</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($filteredHotels as $key => $hotel) { ?>
+                                <tr>
+                                    <th scope="row">
+                                        <?php echo ($key + 1) ?>
+                                    </th>
+                                    <td>
+                                        <?php echo $hotel['name'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['description'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo ($hotel['parking'] ? 'Sì' : 'No'); ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['vote'] ?> &#128970;
+                                    </td>
+                                    <td>
+                                        A <?php echo $hotel['distance_to_center'] ?> Km dal centro
+                                    </td>
+                                </tr>
+                            <?php  } ?>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+    </main>
 
 </body>
 
